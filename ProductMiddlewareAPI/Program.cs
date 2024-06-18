513228be88066ed696790ffcb7aba61bfc852e49
tbogdan <tbogdan@algebra.hr>
2024-06-18 11:18:50 +0200

    Implemented caching

--- ProductMiddlewareAPI/Program.cs
+++ ProductMiddlewareAPI/Program.cs
@@ -1,25 +1,25 @@
 using ProductMiddlewareAPI.Interfaces;
+using ProductMiddlewareAPI.Mapping;
 using ProductMiddlewareAPI.Services;
 using ProductMiddlewareDataAcces.Interfaces;
 using ProductMiddlewareDataAcces.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddMemoryCache();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<ApiProductRepository>();
 builder.Services.AddScoped<IProductRepository, ApiProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddAutoMapper(typeof(ProductProfile));
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- ProductMiddlewareAPI/Services/ProductService.cs
+++ ProductMiddlewareAPI/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
 using ProductMiddlewareAPI.Interfaces;
 using ProductMiddlewareAPI.ViewModels;
 using ProductMiddlewareDataAcces.Interfaces;
@@ -10,34 +11,83 @@ namespace ProductMiddlewareAPI.Services
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
+        private readonly IMemoryCache _memoryCache;
+        private const string ProductsCachKey = "ProductsCach";
 
-        public ProductService(IProductRepository productRepository, IMapper mapper)
+        public ProductService(IProductRepository productRepository, IMapper mapper, IMemoryCache memoryCache)
         {
             _productRepository = productRepository;
             _mapper = mapper;
+            _memoryCache = memoryCache;
         }
 
         public async Task<IEnumerable<ProductVM>> GetAllProductsAsync()
         {
-            var products = await _productRepository.GetAllProductsAsync();
-            return _mapper.Map<IEnumerable<ProductVM>>(products);
+            if (!_memoryCache.TryGetValue(ProductsCachKey, out IEnumerable<ProductVM> products))
+            {
+                var productData = await _productRepository.GetAllProductsAsync();
+                products = _mapper.Map<IEnumerable<ProductVM>>(productData);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+                _memoryCache.Set(ProductsCachKey, products, cacheEntryOptions);
+            }
+            return products;
         }
 
         public async Task<ProductVM> GetProductByIdAsync(int id)
         {
-            var product = await _productRepository.GetProductByIdAsync(id);
-            return _mapper.Map<ProductVM>(product);
+            var cacheKey = $"product_{id}";
+            if (!_memoryCache.TryGetValue(cacheKey, out ProductVM product))
+            {
+                var productData = await _productRepository.GetProductByIdAsync(id);
+                if (productData == null)
+                {
+                    return null;
+                }
+
+                product = _mapper.Map<ProductVM>(productData);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                _memoryCache.Set(cacheKey, product, cacheEntryOptions);
+            }
+            return product;
         }
         public async Task<IEnumerable<ProductVM>> FilterProductAsync(string category, decimal? minPrice, decimal? maxPrice)
         {
-            var products = await _productRepository.FilterProductAsync(category, minPrice, maxPrice);
-            return _mapper.Map<IEnumerable<ProductVM>>(products);
+            var cacheKey = $"products_{category}_{minPrice}_{maxPrice}";
+
+            if (!_memoryCache.TryGetValue(cacheKey, out IEnumerable<ProductVM> products))
+            {
+                var productData = await _productRepository.FilterProductAsync(category, minPrice, maxPrice);
+                products = _mapper.Map<IEnumerable<ProductVM>>(productData);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                _memoryCache.Set(cacheKey, products, cacheEntryOptions);
+            }
+
+            return products;
         }
 
         public async Task<IEnumerable<ProductVM>> SearchProductAsync(string query)
         {
-            var products = await _productRepository.SearchProductsAsync(query);
-            return _mapper.Map<IEnumerable<ProductVM>>(products);
+            var cacheKey = $"products_search_{query}";
+            if (!_memoryCache.TryGetValue(cacheKey, out IEnumerable<ProductVM> products))
+            {
+                var productsData = await _productRepository.SearchProductsAsync(query);
+                products = _mapper.Map<IEnumerable<ProductVM>>(products);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                _memoryCache.Set(cacheKey, products, cacheEntryOptions);
+            }
+
+            return products;
         }
     }
 }
